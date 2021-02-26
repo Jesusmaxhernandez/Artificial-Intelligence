ab730572cfc12282c8eeb33b18f8a91433bea870
Gordep <jgonzalez107@miners.utep.edu>
2021-02-25 18:06:45 -0700

    reads input file

--- pathFinding_V1.py
+++ pathFinding_V1.py
@@ -1,27 +1,10 @@
 import sys
 import map_details
 #import Breadth_First_Search
- 
 
-inMatrix = [[2,4,2,1,4,5,2], 
-            [0,1,2,3,5,3,1], 
-            [2,0,4,4,1,2,4], 
-            [2,5,5,3,2,0,1], 
-            [4,3,3,2,1,0,1]]
-
-x = map_details.Map()
-#x.read_file()
-
-
-print(x.dimensions)
-print(x.goal)
-print(x.matrix)
- 
 def get_args():
 
-    input_file_name = sys.argv[1] # get input file
-    print(input_file_name)
-    print(sys.argv[2])
+    file_name = sys.argv[1] # get input file
 
     if "BFS" == sys.argv[2]:
         algroithim_to_run = 'bfs'
@@ -30,5 +13,10 @@ def get_args():
         algroithim_to_run = 'ids'
     elif 'A*' == sys.argv[2]: 
         algroithim_to_run = 'A*'
+
+    m = map_details.Map()
+    f = open(file_name)
+    m.read_file(f)
+    
 
 get_args()
