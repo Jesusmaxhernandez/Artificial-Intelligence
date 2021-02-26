eaf47a962369f55c5ab19e10dabf94a377a9116c
Jesus Maximino Hernandez <jesusmaxhernandez@gmail.com>
2021-02-25 17:08:33 -0700

    BFS update

--- Breadth_First_Search.py
+++ Breadth_First_Search.py
@@ -1,25 +1,27 @@
 from timeit import default_timer as timer
+import node
 
-class Breadth_First_Search:
+class Breadth_First_Search():
     
     def __init__(self):
-        print("Hi")
-
+        print("hello world")
     
     def bfs_matrix(self, start, goal, matrix):
 
         #Timer Start
         timerStart = timer()
         queue = [(start, [])]
+        #"Closed List"
         exploredNodes = []
         exploredNodes.append(start)
+        #Used to insert valid coordinates into queue
         valid_coords = []
 
         #Order and directions that can can travel up,down,left,right
         directions = list([(-1,0), (1,0), (0,-1), (0,1)]) 
 
         matrixLen = len(matrix)
-        numOfNodes = 1
+        numOfNodesExpandes = 1
         maxNodeMem = 0
 
         while queue: 
@@ -35,10 +37,14 @@ class Breadth_First_Search:
             path.append(node)
             
             if node == goal:
-                print("Number of nodes expanded", numOfNodes)
-                print("Maximimum number of nodes held in memory: ", maxNodeMem)
-                return (path)
+                timerEnd = timer()
 
+                totalTime = timerEnd - timerStart
+                # print("Number of nodes expanded", numOfNodesExpandes)
+                # print("Maximimum number of nodes held in memory: ", maxNodeMem)
+                return (path, numOfNodesExpandes, maxNodeMem, totalTime)
+
+            #Goes through neighbors
             for move in directions:
                 x_coord = node[0] + move[0] #0?
                 y_coord = node[1] + move[1] #3
@@ -47,25 +53,17 @@ class Breadth_First_Search:
                 if x_coord >= 0 and y_coord >= 0 and  x_coord <= matrixLen-1 and y_coord <= matrixLen-1 and matrix[x_coord][y_coord] != 0:
                     valid_coords.append((x_coord,y_coord))
                 
-
+            #Adds unexplored nodes in queue
             for child in valid_coords:
                 # print("This is exploredNodes BEFORE: ", exploredNodes)
                 # print("this is children BEFORE: ", child)
                 if child not in exploredNodes:
                     exploredNodes.append(child)
-                    numOfNodes += 1
+                    numOfNodesExpandes += 1
                     # print("This is exploredNodes: ", exploredNodes)
                     # print("this is children", child)
-                    queue.append((child, path[:]))
-                    
-        return None
-
-
-        
-print(inMatrix)
-path = (bfs_matrix((1, 2), (4,3), inMatrix))
-print("Sequence of coordinates: ", path)
-
+                    queue.append((child, path[:]))  
+        return -1
 
 def findTotalCost(path, matrix):
 
@@ -75,5 +73,8 @@ def findTotalCost(path, matrix):
         totalC += matrix[node[0]][node[1]]
 
     return totalC
-
-print("This is the total cost of the path found: ", findTotalCost(path, inMatrix))
+        
+# print(inMatrix)
+# path = (bfs_matrix((1, 2), (4,3), inMatrix))
+# print("Sequence of coordinates: ", path)
+# print("This is the total cost of the path found: ", findTotalCost(path, inMatrix))
