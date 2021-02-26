3e6d592b770a3cc0ac0420024f2055ca51f1a46f
Gordep <jgonzalez107@miners.utep.edu>
2021-02-25 17:49:57 -0700

    Added argument reading

--- pathFinding_V1.py
+++ pathFinding_V1.py
@@ -1,3 +1,4 @@
+import sys
 import map_details
 #import Breadth_First_Search
  
@@ -8,10 +9,6 @@ inMatrix = [[2,4,2,1,4,5,2],
             [2,5,5,3,2,0,1], 
             [4,3,3,2,1,0,1]]
 
-#print(inMatrix[1,2])
-#dimensions, starting_loc, goal, matrix = map.read_map()
-#Breadth_First_Search.bfs_matrix(starting_loc,goal,matrix
-
 x = map_details.Map()
 x.read_file()
 
@@ -20,10 +17,16 @@ print(x.dimensions)
 print(x.goal)
 print(x.matrix)
 
-#print(rm.__dimensions__())
-#rm.read_file()
 
-#print(inMatrix[path[0]])
-#solution given - [(1, 2), (2, 1), (3, 2), (4, 3)] non zero based
-# zero based - [(0, 1), (1, 0), (2, 1), (3, 2)] 
+def get_args(argv):
+
+    input_file_name = argv[1] # get input file
+    print(input_file_name)
     
+    if 'BFS' == argv[2]:
+        algroithim_to_run = 'bfs'
+        print("BFS!")
+    elif 'IDS' == argv[2]:
+        algroithim_to_run = 'ids'
+    elif 'A*' == argv[2]: 
+        algroithim_to_run = 'A*'
