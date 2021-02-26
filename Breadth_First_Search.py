3a043ca56468bb2f56f509cc4c1a7c83948b69a7
Jesus Maximino Hernandez <jesusmaxhernandez@gmail.com>
2021-02-25 18:16:42 -0700

    Updated BFS

--- Breadth_First_Search.py
+++ Breadth_First_Search.py
@@ -1,10 +1,28 @@
 from timeit import default_timer as timer
-import node
 
 class Breadth_First_Search():
     
     def __init__(self):
-        print("hello world")
+        self.costPath = 0 
+        self.numNodesExp = 0
+        self.maxNodesMem = 0
+        self.time = 0
+        self.pathSeq = []
+
+    def __costPath__(self):
+        return self.costPath
+
+    def __numNodesExp__(self):
+        return self.numNodesExp
+    
+    def __maxNodeMem__(self):
+        return self.maxNodesInMem
+    
+    def __time__(self):
+        return self.time
+    
+    def __pathSeq__(self):
+        return self.pathSeq
     
     def bfs_matrix(self, start, goal, matrix):
 
@@ -39,10 +57,16 @@ class Breadth_First_Search():
             if node == goal:
                 timerEnd = timer()
 
-                totalTime = timerEnd - timerStart
+                totalTime = (timerEnd - timerStart) * 1000
                 # print("Number of nodes expanded", numOfNodesExpandes)
                 # print("Maximimum number of nodes held in memory: ", maxNodeMem)
-                return (path, numOfNodesExpandes, maxNodeMem, totalTime)
+                self.numNodesExp = numOfNodesExpandes
+                self.maxNodesInMem = maxNodeMem
+                self.time = totalTime
+                self.costPath = findTotalCost(path, matrix)
+                self.pathSeq = path
+                return 1
+                #return (path, numOfNodesExpandes, maxNodeMem, totalTime)
 
             #Goes through neighbors
             for move in directions:
@@ -73,8 +97,18 @@ def findTotalCost(path, matrix):
         totalC += matrix[node[0]][node[1]]
 
     return totalC
+
+
         
+
+inMatrix = [[2,4,2,1,4,5,2], 
+            [0,1,2,3,5,3,1], 
+            [2,0,4,4,1,2,4], 
+            [2,5,5,3,2,0,1], 
+            [4,3,3,2,1,0,1]]
+
 # print(inMatrix)
-# path = (bfs_matrix((1, 2), (4,3), inMatrix))
-# print("Sequence of coordinates: ", path)
+b = Breadth_First_Search()
+path = (b.bfs_matrix((1, 2), (4,3), inMatrix))
+print("Sequence of coordinates: ", path)
 # print("This is the total cost of the path found: ", findTotalCost(path, inMatrix))
